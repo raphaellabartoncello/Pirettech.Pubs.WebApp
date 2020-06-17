71ebb26a0ae4768978316cb2d7ea3df6fa97275b
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-06-16 22:29:58 -0300

    Atualização das ActionResult para direcionamento das Views e tratamentos de exceções

--- Pirettech.Pubs.WebApp/Controllers/HomeController.cs
+++ Pirettech.Pubs.WebApp/Controllers/HomeController.cs
@@ -33,11 +33,11 @@ namespace Pirettech.Pubs.WebApp.Controllers
             return View();
         }
 
-        public ActionResult Autor()
-        {
-            return View();
-        }
 
+        /// <summary>
+        /// Devolve uma lista de editoras
+        /// </summary>
+        /// <returns>List of Editora</returns>
         public ActionResult Editora()
         {
             List<Editora> listaEditora = null;
@@ -51,6 +51,67 @@ namespace Pirettech.Pubs.WebApp.Controllers
             }
             //Retorna uma lista tipada do tipo Editora, pois a View espera receber um IEnumerable (é uma lista mas não permite alterações)
             return View(listaEditora);
+        }
+
+
+        //Este método retorna os livros relacionados com o ID da Editora enviada pela Página HTML
+        public ActionResult LivrosPorEditora(int id)
+        {
+            List<Livro> listaDeLivros = null;
+            try
+            {
+                if (id <= 0)
+                {
+                    throw new Exception("A consulta solicitada é inválida.");
+                }
+
+                listaDeLivros = BibliotecaDB.LivrosPorEditora(id.ToString());
+
+                if (listaDeLivros == null)
+                {
+                    throw new Exception("A Editora consultada é inválida.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            //Retorna uma lista tipada do tipo Editora, pois a View espera receber um IEnumerable (é uma lista mas não permite alterações)
+            return View(listaDeLivros);
+        }
+
+
+        public ActionResult Autor()
+        {
+            List<Autor> listaAutor = null;
+
+            try
+            {
+                listaAutor = BibliotecaDB.AutoresLista();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            return View(listaAutor);
+        }
+
+        public ActionResult DetalhesAutor(string id)
+        {
+            Autor autor = null;
+
+            try
+            {
+                autor = BibliotecaDB.AutorPorId(id);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            return View(autor);
         }
     }
 }
