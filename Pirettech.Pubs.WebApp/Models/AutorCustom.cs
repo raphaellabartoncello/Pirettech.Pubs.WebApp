7174a1d2dbe0007c68afe18b5ec392730c1222a1
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-06-16 22:26:51 -0300

    Implantação da classe partial para unir nome e sobrenome do autor

---
+++ Pirettech.Pubs.WebApp/Models/AutorCustom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pirettech.Pubs.WebApp.Models
+{
+    public partial class Autor
+    {
+
+        /// <summary>
+        /// Propriedade READONLY - Retorna nome completo do autor
+        /// </summary>
+        public string NomeCompleto
+        {
+            get { return Nome + " " + Sobrenome; }
+        }
+    }
+}
